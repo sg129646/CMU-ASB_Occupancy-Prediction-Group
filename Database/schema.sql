a907a0fb7e61cd4a9779bbc7bce3ceb2cce391ec
Weronika-Przedworska <144631559+Weronika-Przedworska@users.noreply.github.com>
2026-04-13 19:47:03 -0400

    Update schema.sql
    Update schema to match existing sensor code

--- Database/schema.sql
+++ Database/schema.sql
@@ -1,19 +1,3 @@
-CREATE TABLE rooms (
-    room_number       SERIAL PRIMARY KEY,
-    name     TEXT NOT NULL,
-    capacity INTEGER
-);
-
-CREATE TABLE classes (
-    class_id          SERIAL PRIMARY KEY,
-    room_id     INTEGER REFERENCES rooms(room_number),
-    name        TEXT NOT NULL,
-    day_of_week INTEGER,
-    start_time  TIME,
-    end_time    TIME,
-    rating      REAL
-);
-
 CREATE TABLE weather (
     timestamp     TIMESTAMPTZ PRIMARY KEY,
     temperature   REAL,
@@ -23,10 +7,36 @@ CREATE TABLE weather (
     condition     INT
 );
 
+CREATE TABLE rooms (
+    room         TEXT PRIMARY KEY,
+    name         TEXT NOT NULL,
+    capacity     INTEGER
+);
 
-CREATE TABLE sensor_readings (
+CREATE TABLE classes (
+    class_id     SERIAL PRIMARY KEY,
+    room         TEXT REFERENCES rooms(room),
+    name         TEXT NOT NULL,
+    day_of_week  INTEGER,
+    start_time   TIME,
+    end_time     TIME,
+    weekly_hours REAL
+);
+
+CREATE TABLE events (
     id              SERIAL PRIMARY KEY,
-    room_number         INTEGER REFERENCES rooms(room_number),
+    room            TEXT REFERENCES rooms(room),
     timestamp       TIMESTAMPTZ DEFAULT NOW(),
-    occupancy_count INTEGER
+    direction       TEXT,
+    total_in        INTEGER DEFAULT 0,
+    total_out       INTEGER DEFAULT 0,
+    occupancy       INTEGER
+);
+
+CREATE TABLE room_state (
+    room         TEXT PRIMARY KEY REFERENCES rooms(room),
+    occupancy    INTEGER NOT NULL DEFAULT 0,
+    total_in     INTEGER NOT NULL DEFAULT 0,
+    total_out    INTEGER NOT NULL DEFAULT 0,
+    last_update  TIMESTAMPTZ NOT NULL
 );
